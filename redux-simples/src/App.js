9b3ad724b4e4793d59ba22519119b9ca58bc9c29
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-09-22 20:57:01 -0300

    Iniciando a App

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import Card from './components/Card';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Exercício React-Redux (Simples)</h1>
+
+      <div className="linha">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
+      <div className="linha">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
---
+++ redux-simples/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import './Card.css';
+import React from 'react';
+
+const Card =  (props) => {
+    return (
+        <div>
+            Card
+        </div>
+    )
+}
+
+export default Card;
