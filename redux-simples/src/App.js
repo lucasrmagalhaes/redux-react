01f77c45267e762abc433ec2b5f0927744687dd4
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-09-22 21:16:59 -0300

    Componente Card

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -8,15 +8,13 @@ function App() {
       <h1>Exercício React-Redux (Simples)</h1>
 
       <div className="linha">
-        <Card />
-        <Card />
-        <Card />
+        <Card title="Card 1" red>X</Card>
+        <Card title="Card 2" green>X</Card>
       </div>
 
       <div className="linha">
-        <Card />
-        <Card />
-        <Card />
+        <Card title="Card 3" blue>Y</Card>
+        <Card title="Card 4" purple>X</Card>
       </div>
     </div>
   );
--- redux-simples/src/components/Card.jsx
+++ redux-simples/src/components/Card.jsx
@@ -1,10 +1,28 @@
-import './Card.css';
 import React from 'react';
+
+import './Card.css';
+
+function getColor({ red, green, blue, purple }) {
+    if (red) return "Red";
+    if (green) return "Green";
+    if (blue) return "Blue";
+    if (purple) return "Purple";
+
+    return "";
+}
 
 const Card =  (props) => {
     return (
-        <div>
-            Card
+        <div className={`Card ${getColor(props)}`}>
+            <div className='Header'>
+                <span className='Title'>
+                    {props.title}
+                </span>
+            </div>
+
+            <div className='Content'>
+                {props.children}
+            </div>
         </div>
     )
 }
